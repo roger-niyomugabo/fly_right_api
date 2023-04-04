314bb8b30e5306a22179b7449399d0df27e9a11b
Niyonkurutresor <95636719+Niyonkurutresor@users.noreply.github.com>
2023-04-04 11:41:52 +0200

    Update admin.js

--- src/middlewares/validation/admin.js
+++ src/middlewares/validation/admin.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import validator from '../../helpers/validator';
 import adminSignupSchema from './schemas/admin/signup';
+import adminSigninSchema from './schemas/admin/signin';
 
 export const adminSignupValidation = (req, res, next) => (
   validator(adminSignupSchema, req.body, res, next)
