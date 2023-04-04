a2e9ef6d04dbef9c1b4a221bffd04190c5e344f3
niyonkurutresor <niyonkurutresor17@gmail.com>
2023-04-04 10:22:58 +0200

    Implement secure login functionality with password encryption and two-factor authentication with jwt

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,4 +1,4 @@
-import { generate } from '../helpers/bcrypt';
+import { generate, check } from '../helpers/bcrypt';
 import output from '../helpers/response';
 import AdminService from '../database/services/adminServices';
 import { sign } from '../helpers/jwt';
@@ -25,6 +25,20 @@ class AdminController {
       admin.password = undefined;
       const token = await sign({ _id: admin._id, role: 'admin' }, { expiresIn: '72h' });
       return output(res, 201, 'Signed up successfully', { admin, token });
+    } catch (error) {
+      return output(res, 500, error.message || error, null, 'SERVER_ERROR');
+    }
+  }
+
+  static async signinAdmin(req, res) {
+    try {
+      const { email, password } = req.body;
+      const adminExist = await AdminService.findAdmin({ email });
+      if (!adminExist) return output(res, 409, 'Please create an acount', null, 'THEREIS_NO_ACCOUNT');
+      const isMatch = await check(adminExist.password, password);
+      if (!isMatch) return output(res, 409, 'You are not allowed to access this account', null, 'WRONG_PASSWORD');
+      const token = await sign({ _id: adminExist._id, role: 'admin' }, { expiresIn: '72h' });
+      return output(res, 200, 'You loged in successfully', { adminExist, token });
     } catch (error) {
       return output(res, 500, error.message || error, null, 'SERVER_ERROR');
     }
--- src/middlewares/validation/admin.js
+++ src/middlewares/validation/admin.js
@@ -5,3 +5,7 @@ import adminSignupSchema from './schemas/admin/signup';
 export const adminSignupValidation = (req, res, next) => (
   validator(adminSignupSchema, req.body, res, next)
 );
+
+export const adminSigninValidation = (req, res, next) => (
+  validator(adminSigninSchema, req.body, res, next)
+);
