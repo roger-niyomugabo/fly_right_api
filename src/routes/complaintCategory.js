923916cdb489e3a5b78caa1c29038eeeedf2be55
roger-niyomugabo <rogerniyomugabo3@gmail.com>
2023-05-15 08:33:45 +0200

    get all complaint descriptions

--- src/routes/complaintCategory.js
+++ src/routes/complaintCategory.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import ComplaintCategoryController from '../controllers/complaintCategoryController';
 import * as Validation from '../middlewares/validation/complaintCategory';
 import { isAdmin } from '../middlewares/authorization';
+import ComplaintDescriptionController from '../controllers/complaintDescriptionController';
 
 const router = Router();
 
 router.post('/create', isAdmin, Validation.complaintCategoryValidation, ComplaintCategoryController.create);
+router.get('/:_id/descriptions', ComplaintDescriptionController.allDescriptions);
 
 export default router;
